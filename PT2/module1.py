6ebbefeaae15b9148114445afc8e1b9604dd8c4e
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-02-16 17:43:28 +0800

    malinis na version ng gawa ko

--- PT2/module1.py
+++ PT2/module1.py
@@ -1,36 +1,35 @@
 # Module 1 as customer, the user is a customer - able to buy from the lists below
 
+#Lists of product and their respective prices
+dairy = ["Fresh Milk", "NAN Gold", "Promil Gold"]
+dairyPrice = [150.0, 1590.0, 1480.0]
+wetGoods = ["Ground Beef", "Chicken Wings", "Pork Knuckles"]
+wetGoodsPrice = [450.0, 280.0, 350.0]
+
+# List for customer's purchase
+custCart = []
+cartValue = []
+multiplier = []
+
 # This function lets me print product lists easily
 def stockPrinter(num, product, price,len):
     while num != len:
         print("|", num+1 ,"|" + product[num] + ":\t\t₱", price[num])
         num += 1
 
-dairy = ["Fresh Milk", "NAN Gold", "Promil Gold"]
-dairyPrice = [150.0, 1590.0, 1480.0]
-
-wetGoods = ["Ground Beef", "Chicken Wings", "Pork Knuckles"]
-wetGoodsPrice = [450.0, 280.0, 350.0]
-
-print("\nWelcome, our dear customer!")
-
-custCart = []
-cartValue = []
-multiplier = []
-
 # This function is for taking user's order
 def custBuying(custCart,cartValue,multiplier):
 
     print("\nAVAILABLE PRODUCTS:")
-    print("\n(0) DAIRY\n")
+    print("\n(1) DAIRY\n")
     stockPrinter(0,dairy,dairyPrice,len(dairy))
-    print("\n(1) WET GOODS\n")
+    print("\n(2) WET GOODS\n")
     stockPrinter(0,wetGoods,wetGoodsPrice,len(wetGoods))
 
     while True:
         productGroup = input("*Enter the number code of product category: ")
 
-        if productGroup == '0':
+        if productGroup == '1':
             order = int(input("*Enter the number code of your order: "))
             quantity = int(input("*Enter quantity of products: "))
             order -= 1
@@ -38,11 +37,11 @@ def custBuying(custCart,cartValue,multiplier):
             cartValue.append(dairyPrice[order])
             multiplier.append(quantity)
 
-            print("You added ", quantity ," " + dairy[order] + " to your cart")
+            print("\nYou added ", quantity ," " + dairy[order] + " to your cart")
             print(multiplier[-1], custCart[-1],":\t₱", cartValue[-1]*quantity)
 
 
-        elif productGroup == '1':
+        elif productGroup == '2':
             order = int(input("*Enter the number code of your order: "))
             quantity = int(input("*Enter quantity of products: "))
             order -= 1
@@ -50,7 +49,7 @@ def custBuying(custCart,cartValue,multiplier):
             cartValue.append(wetGoodsPrice[order])
             multiplier.append(quantity)
 
-            print("You added ", quantity ," " + wetGoods[order] + " to your cart")
+            print("\nYou added ", quantity ," " + wetGoods[order] + " to your cart")
             print(multiplier[-1], custCart[-1],":\t₱", cartValue[-1]*quantity)
 
         else:
@@ -65,18 +64,22 @@ def custBuying(custCart,cartValue,multiplier):
         else:
             custBuying(custCart,cartValue,multiplier)
             
-# This function is to finalize user's orders
+# This function is shows the total price of the products ordered
 def custCheckout(product,price,multiplier):
     num = 0
     totalPrice = 0
-    print("\n")
+    print("\nHere are your purchases:")
 
     while num != len(product):
         print("|",multiplier[num],"|" + product[num] + ":\t\t₱", price[num]*multiplier[num])
         totalPrice = totalPrice + (price[num]*multiplier[num])
         num += 1
 
-    print("Total Price: ₱", totalPrice)
-    input("*Press any key to end the program.")
+    print("Total Price: \t₱", totalPrice)
+    input("\n*Press any key to end the program.")
+    print("\nProgram Ended")
 
+
+# START of this module
+print("\nWelcome, our dear customer!")
 custBuying(custCart,cartValue,multiplier)
--- PT2/module2.py
+++ PT2/module2.py
@@ -1,4 +1,120 @@
 # Module 2 as merchandiser, the user is a Merchandiser - able to edit, add, and remove items on the list
-from main import dairyLists
-from main import wetGoodsLists
-print("hmm?")
+
+# This function does what it does
+def productChooser(action):
+    dairy = ["Fresh Milk", "NAN Gold", "Promil Gold"]
+    dairyPrice = [150.0, 1590.0, 1480.0]
+
+    wetGoods = ["Ground Beef", "Chicken Wings", "Pork Knuckles"]
+    wetGoodsPrice = [450.0, 280.0, 350.0]
+    
+    print("\n||DAIRY - 1|\t|WET GOODS - 2||")
+    product = int(input("\n*Enter product category number: "))
+
+    if product == 1:
+        if action == 1:
+            editProduct(dairy,dairyPrice)
+
+        elif action == 2:
+            addProduct(dairy,dairyPrice)
+
+        elif action == 3:
+            remProduct(dairy,dairyPrice)
+    
+        else:
+            action()
+
+    elif product == 2:
+
+        if action == 1:
+            editProduct(wetGoods,wetGoodsPrice)
+
+        elif action == 2:
+            addProduct(wetGoods,wetGoodsPrice)
+
+        elif action == 3:
+            remProduct(wetGoods,wetGoodsPrice)
+
+        else:
+            action()
+        
+    else:
+        print("\nInvalid input.")
+        productChooser()
+
+# This function lets you edit product in a list
+def editProduct(product,price):
+
+    print("\nCurrent inventory")
+    stockPrinter(0,product,price,len(product))
+
+    index = int(input("*Enter the number code to be changed: ")) - 1
+    changeProduct = input("*Enter new product: ")
+    changeProductPrice = float(input("*Enter price: "))
+
+    product.pop(index)
+    price.pop(index)
+    product.insert(index,changeProduct)
+    price.insert(index,changeProductPrice)
+
+    print("\nCurrent inventory")
+    stockPrinter(0,product,price,len(product))
+
+    endProgram()
+
+# This function lets you add product in a list
+def addProduct(product,price):
+
+    newProduct = input("\n*Enter new product: ")
+    newProductPrice = float(input("*Enter price: "))
+
+    product.append(newProduct)
+    price.append(newProductPrice)
+
+    print("\nCurrent inventory")
+    stockPrinter(0,product,price,len(product))
+
+    endProgram()
+            
+# This function lets you remove product in a list
+def remProduct(product,price):
+
+    print("\nCurrent inventory")
+
+    stockPrinter(0,product,price,len(product))
+
+    index = int(input("*Enter the number code to be removed: ")) - 1
+
+    product.pop(index)
+    price.pop(index)
+
+    print("\nCurrent inventory")
+    stockPrinter(0,product,price,len(product))
+
+    endProgram()
+    
+# This function lets you choose your action
+def actions():
+    print("\n||EDIT - 1|\t|ADD - 2|\t|REMOVE - 3||")
+    action = int(input("\n*Enter inventory options: "))
+
+    if action == 1 or 2 or 3:
+        productChooser(action)
+
+    else: 
+        print("\nInvalid input.")
+        actions()
+
+# This function prints the lists 
+def stockPrinter(num, product, price,len):
+    while num != len:
+        print("|", num+1 ,"|" + product[num] + ":\t\t₱", price[num])
+        num += 1
+        
+def endProgram():
+    input("\n*Press enter to end the program.\n")
+    print("Program Ended")
+
+# START of the module
+print("\nWelcome, master!")
+actions()
