b64955f4f4b3394ffcf242f3c6c486a8bb48da36
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-02-23 11:11:10 +0800

    Add files via upload

---
+++ PT3/main.py
@@ -0,0 +1,41 @@
+import pizza
+
+addOns = {
+    "Cheese" : 100,
+    "Mozzarella Cheese" : 150,
+    "Pepper" : 80,
+    "Bacon Ham" : 120,
+    "Mushroom" : 130,
+    "Onions Chili" : 110,
+    "Tomato" : 90,
+    "Pineapple" : 100,
+    "Salami" : 120
+    }
+
+def finalOutput(finalPrice):
+    finalPizzaChoice = {
+        "Customer" : custName.capitalize(),
+        "Pizza Preference": prefPizza.capitalize(),
+        "Bill" : finalPrice
+    }
+    
+    print("\nCustomer: " + finalPizzaChoice["Customer"])
+    print("Pizza Preference: " + finalPizzaChoice["Pizza Preference"])
+    print("Bill: " + str(finalPizzaChoice["Bill"]))
+
+custName = input("*Enter the Customers Name: ").strip()
+prefPizza = input("*Preferred Pizza: ").strip().lower()
+base = 450
+print("Details:\nBase: " + str(base))
+
+
+if prefPizza == "deluxe":
+    finalOutput(pizza.deluxeAddOns(base, addOns["Cheese"], addOns["Bacon Ham"], addOns["Onions Chili"]))
+
+elif prefPizza == "special":
+    finalOutput(pizza.specialAddOns(base, addOns["Cheese"], addOns["Pepper"], addOns["Bacon Ham"],addOns["Mushroom"], addOns["Onions Chili"]))
+
+elif prefPizza == "primo":
+    finalOutput(pizza.primoAddOns(base, addOns["Mozzarella Cheese"], addOns["Pepper"], addOns["Bacon Ham"], addOns["Mushroom"], addOns["Onions Chili"], addOns["Tomato"], addOns["Pineapple"], addOns["Salami"]))
+
+
---
+++ PT3/pizza.py
@@ -0,0 +1,17 @@
+def deluxeAddOns(base,a,b,c):
+    print("Cheese\nBacon Ham\nOnions Chili ")
+    addOns = a + b + c
+    finalPrice = base + addOns
+    return finalPrice
+    
+def specialAddOns(base,a,b,c,d,e):
+    print("Cheese\nPepper\nBacon Ham\nMushroom\nOnions Chili")
+    addOns = a + b + c + d + e
+    finalPrice = base + addOns
+    return finalPrice
+
+def primoAddOns(base,a,b,c,d,e,f,g,h):
+    print("Mozzarella Cheese\nPepper\nBaconHam\nMushroom\nOnions Chili\nTomato\nPineapple\nSalami")
+    addOns = a + b + c + d + e + f + g + h
+    finalPrice = base + addOns
+    return finalPrice
